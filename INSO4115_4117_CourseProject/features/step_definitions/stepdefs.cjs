5b8cae049b2f7387755dc46883bc25e32a8084fe
EdwinC1339 <edwin.camuy@upr.edu>
2023-03-09 14:02:38 -0400

    Dummy step defs

--- INSO4115_4117_CourseProject/features/step_definitions/stepdefs.cjs
+++ INSO4115_4117_CourseProject/features/step_definitions/stepdefs.cjs
@@ -1,2 +1,22 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('a patron has vegan as a restriction', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Given('the specific restaurant does not have vegan options', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+When('they request to know if the restaurant has vegan options', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then('they {string} attend the restaurant', function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
