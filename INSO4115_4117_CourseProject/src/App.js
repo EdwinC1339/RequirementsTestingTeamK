91aa39e8fc071989e2ead10e8ecda35d877c3804
AcevedoC17 <carlos.acevedo17@upr.edu>
2023-03-09 14:03:02 -0400

    fixed compile error

--- INSO4115_4117_CourseProject/src/App.js
+++ INSO4115_4117_CourseProject/src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import React, { Component } from "react";
 
 function App() {
   return (
