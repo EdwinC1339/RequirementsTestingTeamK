3b3973a9d0eec452e60d3c57101b9bf4d5d9e022
EdwinC1339 <edwin.camuy@upr.edu>
2023-03-15 02:11:02 -0400

    Got google map working in home page

--- INSO4115_4117_CourseProject/src/components/Map.jsx
+++ INSO4115_4117_CourseProject/src/components/Map.jsx
@@ -1,7 +1,51 @@
-import React, { Component } from "react";
-import "./styles/styles.css";
-export default class Map extends Component {
-  render() {
-    return <div className="map-div"></div>;
-  }
+import React from 'react'
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map)
+  }, [])
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+  ) : <></>
 }
+
+export default React.memo(Map)
