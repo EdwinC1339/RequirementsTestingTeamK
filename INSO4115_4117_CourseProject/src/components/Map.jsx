fac7173b4320d898d1e29744f29aa2b1245059bd
EdwinC1339 <edwin.camuy@upr.edu>
2023-03-15 03:05:53 -0400

    Map zooms on PR

--- INSO4115_4117_CourseProject/src/components/Map.jsx
+++ INSO4115_4117_CourseProject/src/components/Map.jsx
@@ -10,8 +10,8 @@ const containerStyle = {
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 18.266,
+  lng: -66.406
 };
 
 function Map() {
@@ -25,7 +25,6 @@ function Map() {
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
 
     setMap(map)
   }, [])
@@ -38,7 +37,7 @@ function Map() {
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={10}
+        zoom={9}
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
