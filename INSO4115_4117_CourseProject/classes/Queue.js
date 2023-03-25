08706104be9b95661f1a065fea3755d66e0c23fc
EdwinC1339 <edwin.camuy@upr.edu>
2023-03-25 10:07:12 -0400

    Queue and promise queue

---
+++ INSO4115_4117_CourseProject/classes/Queue.js
@@ -0,0 +1,32 @@
+export default class Queue {
+    constructor() {
+      this.items = [];
+    }
+  
+    enqueue(item) {
+      this.items.push(item);
+    }
+  
+    dequeue() {
+      if (this.isEmpty()) {
+        return null;
+      }
+      return this.items.shift();
+    }
+  
+    peek() {
+      if (this.isEmpty()) {
+        return null;
+      }
+      return this.items[0];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  }
+  
