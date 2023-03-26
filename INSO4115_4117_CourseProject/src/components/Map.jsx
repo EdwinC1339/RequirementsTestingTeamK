7bddabe2cf4f17b3e897b23e4f74521beb318fa3
EdwinC1339 <edwin.camuy@upr.edu>
2023-03-26 00:43:39 -0400

    WIP map changes to use DB insted of Google API

--- INSO4115_4117_CourseProject/src/components/Map.jsx
+++ INSO4115_4117_CourseProject/src/components/Map.jsx
@@ -24,6 +24,48 @@ const center = {
   lng: -66.406,
 };
 
+// This function costs money to run so avoid using it
+// Just keeping it here so we can get more restaurants later
+const searchPlaces = (setPlaces, usePagination, mapInstance) => {
+  const width = 0.8;
+  const xSegments = 40;
+  const height = 0.2;
+  const ySegments = 20;
+  for (let xoffset = -width; xoffset < width; xoffset += width / xSegments * 2) {
+    for (let yoffset = -height; yoffset < height; yoffset += height / ySegments * 2) {
+      const placesService = new window.google.maps.places.PlacesService(mapInstance);
+      const location = {
+        lat: center.lat + yoffset,
+        lng: center.lng + xoffset
+      }
+      const request = {
+        location,
+        radius: 5000, // Search within a 1000-meter radius
+        type: 'restaurant',
+      };
+  
+      placesService.nearbySearch(request, (results, status, pagination) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+
+          const placesDict = {}
+          results.forEach((place) =>{
+            placesDict[place.place_id] = place
+          })
+
+          setPlaces(prevPlaces => Object.assign({}, prevPlaces, placesDict));
+          if (pagination.hasNextPage && usePagination) {
+            pagination.nextPage();
+          }
+        } else {
+          console.error(status)
+        }
+        
+      });
+
+    }
+  }
+}
+
 function Map() {
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -31,45 +73,15 @@ function Map() {
   });
 
   const [places, setPlaces] = useState({});
+  const [usePagination, setUsePagination] = useState(false);
 
   const onLoad = (mapInstance) => {
-    const placesService = new window.google.maps.places.PlacesService(mapInstance);
-
-    const width = 1
-    const xSegments = 3
-    const height = 0.3
-    const ySegments = 7
-    for (let xoffset = -width; xoffset < width; xoffset += width / xSegments * 2) {
-      for (let yoffset = -height; yoffset < height; yoffset += height / ySegments * 2) {
-        const location = {
-          lat: center.lat + yoffset,
-          lng: center.lng + xoffset
-        }
-        const request = {
-          location,
-          radius: 100000, // Search within a 1000-meter radius
-          type: 'restaurant',
-        };
-    
-        placesService.nearbySearch(request, (results, status, pagination) => {
-          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-
-            const placesDict = {}
-            results.forEach((place) =>{
-              placesDict[place.place_id] = place
-            })
-
-            setPlaces(prevPlaces => Object.assign({}, prevPlaces, placesDict));
-            if (pagination.hasNextPage) {
-              pagination.nextPage();
-            }
-          } else {
-            console.error(status)
-          }
-          
-        });
-
-      }
+    // Keep this set to true. When it's false, it will search all the places from the Google API which is expensive
+    const useDBPlaces = true;
+    if (useDBPlaces) {
+      
+    } else {
+      // searchPlaces(setPlaces, usePagination, mapInstance);
     }
 
   };
@@ -79,6 +91,7 @@ function Map() {
       <Marker
         key={place.place_id}
         position={place.geometry.location}
+        onMouseOver={() => console.log(place.name)}
       />
     ));
   };
