ea7452112afe330806dfb463226f5a025c9bcaec
EdwinC1339 <edwin.camuy@upr.edu>
2023-04-02 10:06:14 -0400

    matching each POI to its municipality

--- INSO4115_4117_CourseProject/src/components/Map.js
+++ INSO4115_4117_CourseProject/src/components/Map.js
@@ -5,19 +5,37 @@ import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "!mapbox-gl";
 import 'mapbox-gl/dist/mapbox-gl.css'
 import "./Map.css";
+import {municipalities} from './municipalities.json'
 
 const key =
   "pk.eyJ1IjoiYWxvbnNvMTQiLCJhIjoiY2xmcm1scDM0MDVpMjN6bDhnenhleDI0dyJ9.WCGBtiA1Ij0EkiA6IpOgrA";
 mapboxgl.accessToken = key;
 
+//18.229781, -66.390552
+
 const center = {
-  lat:  18.46633000,
-  lng: -66.10572000,
+  lat:  18.229781,
+  lng: -66.390552,
 };
 
+async function fetchWithTimeoutAndDelay(resource, options = {}) {
+  const { timeout, delay } = options;
+  
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout + delay);
+  const response = await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      fetch(resource, {
+        ...options,
+        signal: controller.signal  
+      }).then(res => resolve(res)).catch(err => reject(err));
+    }, delay)
+  })
+  clearTimeout(id);
+  return response;
+}
 
-
-async function getPlaces(lat, lon, radius) {
+async function getPlaces(lat, lon, radius, delay) {
   let apiURL =
     "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/" +
     lon +
@@ -28,7 +46,7 @@ async function getPlaces(lat, lon, radius) {
     key;
 
   try {
-    let result = (await fetch(apiURL, {method: "GET"})).json()
+    let result = (await fetchWithTimeoutAndDelay(apiURL, {delay, timeout: 30000, method: "GET"})).json()
     return result;
   } catch (error) {
     console.error(error);
@@ -38,59 +56,64 @@ async function getPlaces(lat, lon, radius) {
 
 
 
-async function searchPlaces(setPlaces, Map)   {
-  //console.log("HOOPLA!!!!!!!!!!!!!!!!!!!!")
+async function searchPlaces(setPlaces)   {
   const width = 0.8;
-  const xSegments = 80;
-  const height = 0.2;
-  const ySegments = 50;
-  const placesDict = {}
+  const xSegments = 40;
+  const height = 0.195;
+  const ySegments = 25;
+  let delay = 0;
   for (let xoffset = -width; xoffset < width; xoffset += (width / xSegments) * 2) {
     for (let yoffset = -height; yoffset < height; yoffset += (height / ySegments) * 2) {
       
       let lat = center.lat + yoffset;
       let lon = center.lng + xoffset;
    
-      let result = await getPlaces(lat, lon, "1000")
-      for(let i = 0; i < result.features.length; i++){
-        
-        if (result.features[i].properties.class === "food_and_drink"){
-          const el = document.createElement('div')
-          el.className = 'marker'
-          new mapboxgl.Marker(el).setLngLat(result.features[i].geometry.coordinates).addTo(Map)
-          console.log(result.features[i]);
-          //check if rest. with result.features[i].properties.category == "restaurant" ?? 
-          placesDict[result.features[i].place_id] = result.features[i].properties.name;
-
-          //addMarker(result.features[i]);
+      getPlaces(lat, lon, "10000", delay).then(result => {
+        for(let i = 0; i < result.features.length; i++){
+      
+          if (result.features[i].properties.class === "food_and_drink"){
+            //check if rest. with result.features[i].properties.category == "restaurant" ?? 
+            const item = {[result.features[i].id] : result.features[i]}
+            setPlaces(places => Object.assign({}, places, item));
+          }
+            
         }
-          
-      }
-      
-      setPlaces(prevPlaces => ({...prevPlaces, ...placesDict}));  
+      }).catch(err => console.error(err))
     }
-      
+    delay += ySegments * 30; // 30ms delay per query.
   }
-  console.log('done!!!!!!!!!!')
-  console.log(Object.keys(placesDict).length);
-};
-
-const addMarker = async feature => {
-
-  const marker =  new mapboxgl.Marker()
-        .setLngLat([feature.geometry.coordinates[0], feature.geometry.coordinates[1]])
-        .addTo(Map);
 }
+
+async function getTown(coordinates) {
+  const longitude = coordinates[0];
+  const latitude = coordinates[1];
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${key}`;
+  const response = await fetchWithTimeoutAndDelay(url, {timeout: 30000, delay: 0});
+  
+  const body = await response.json();
+  const address = body.features[0].place_name;
+  const address_arr = address.split(",");
+  let town = address_arr[address_arr.length - 2].trim();
+  if (town === "Puerto Rico") {
+    town = address_arr[address_arr.length - 3].trim();
+  }
+  return town;
+}
+
 const Map = () => {
   const mapContainerRef = useRef(null);
+  const [map, setMap] = useState(null);
   const [lng, setLng] = useState(-66.4411);
   const [lat, setLat] = useState(18.28);
   const [zoom, setZoom] = useState(8.5);
   const [places, setPlaces] = useState({}) // initially empty
+  const pbm = {};
+  municipalities.forEach((m) => pbm[m] = []);
+  const [placesByMunicipality, setPlacesByMunicipality] = useState(pbm) // Maps from each of the 78 municipality to an array of places
 
   // Initialize map when component mounts
   useEffect(() => {
-    const map = new mapboxgl.Map({
+    const m = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/mapbox/streets-v12",
       center: [lng, lat],
@@ -98,59 +121,45 @@ const Map = () => {
       projection: { name: "mercator" }
     });
 
+    setMap(m)
+
     // Add navigation control (the +/- zoom buttons)
-    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+    m.addControl(new mapboxgl.NavigationControl(), "top-right");
 
-    map.on("move", () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
+    m.on("move", () => {
+      setLng(m.getCenter().lng.toFixed(4));
+      setLat(m.getCenter().lat.toFixed(4));
+      setZoom(m.getZoom().toFixed(2));
     });
 
-    map.on("load", function() {
-      // const geojson = {
-      //   'type': 'FeatureCollection',
-      //   'features': [
-      //   {
-      //   'type': 'Feature',
-      //   'geometry': {
-      //   'type': 'Point',
-      //   'coordinates': [-66.10572000, 18.46633000]
-      //   },
-      //   'properties': {
-      //   'title': 'Mapbox',
-      //   'description': 'San Francisco, California'
-      //   }
-      //   }
-      //   ]
-      //   };
-      
-      // const el = document.createElement('div');
-      // el.className = 'marker';
-      // new mapboxgl.Marker(el).setLngLat(geojson.features[0].geometry.coordinates).setPopup(new mapboxgl.Popup({offset:25}).setHTML(
-      //   `<h3>${geojson.features[0].properties.title}</h3><p>${geojson.features[0].properties.description}</p>`
-      //   )).addTo(map)
-      // console.log(map)
-      map.resize();
-      searchPlaces(setPlaces, map);
-      
+    m.on("load", function() {
+      console.log(municipalities)
+      m.resize();
+      searchPlaces(setPlaces);      
     });
-
-    // map.on("click", function(ev) {
-    //   $.get(
-    //     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
-    //       ev.lngLat.lon + "," + ev.lngLat.lat + ".json?access_token=" + mapboxgl.accessToken,
-    //     function(data) {
-    //       console.log(data);
-    //     }
-    //   ).fail(function(jqXHR, textStatus, errorThrown) {
-    //     alert("There was an error while geocoding: " + errorThrown);
-    //   });
-    // });
 
     // Clean up on unmount
-    return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    return () => m.remove();
+  }, [mapContainerRef]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    const markers = []
+    Object.values(places).forEach(place => {
+      const el = <div className='marker'></div>;
+      const marker = new mapboxgl.Marker(el).setLngLat(place.geometry.coordinates).addTo(map);
+      markers.push(marker);
+      getTown(place.geometry.coordinates)
+      .then((town) =>
+        setPlacesByMunicipality(prevState => {
+          const prevTownContent = prevState[town];
+          const newPlaces = [...prevTownContent, place];
+          return Object.assign({}, prevState, {[town]: newPlaces});
+        })
+      )
+      .catch(err => console.error(err));
+    })
+   return () => {markers.forEach(marker => marker.remove())}
+  }, [places])
 
   return (
     <div>
