a331417c7b20054b7f501eb36d8dee8516d9fd5d
EdwinC1339 <edwin.camuy@upr.edu>
2023-04-03 01:06:40 -0400

    Wrote unit tests

--- INSO4115_4117_CourseProject/test/queue.test.js
+++ INSO4115_4117_CourseProject/test/queue.test.js
@@ -1,28 +1,35 @@
 import Queue from '../src/classes/Queue.js'
 
-const q1 = new Queue();
+test('Q1', () => {
+    const q1 = new Queue();
 
-q1.enqueue(1);
-q1.enqueue("Maine");
+    q1.enqueue(1);
+    q1.enqueue("Maine");
 
-console.assert(q1.peek() === 1);
-console.assert(q1.dequeue() === 1);
-console.assert(q1.dequeue() === "Maine");
+    expect(q1.peek()).toBe(1);
+    expect(q1.dequeue()).toBe(1);
+    expect(q1.dequeue()).toBe("Maine");
+})
 
-const q2 = new Queue();
+test('Q2', () => {
+    const q2 = new Queue();
 
-console.assert(q2.dequeue() === null);
-console.assert(q2.dequeue() === null);
+    expect(q2.dequeue()).toBe(null);
+    expect(q2.dequeue()).toBe(null);
+    
+    q2.enqueue(2);
+    q2.enqueue(5);
+    
+    expect(q2.dequeue()).toBe(2);
+    
+    q2.enqueue(30);
+    
+    expect(q2.dequeue()).toBe(5);
 
-q2.enqueue(2)
-q2.enqueue(5)
+    q2.enqueue(35);
 
-console.assert(q2.dequeue() === 2)
+    expect(q2.dequeue()).toBe(30);
+    
+})
 
-q2.enqueue(30)
 
-console.assert(q2.dequeue() === 5)
-
-q2.enqueue(35)
-
-console.assert(q2.dequeue() === 30)
--- INSO4115_4117_CourseProject/test/restaurant.test.js
+++ INSO4115_4117_CourseProject/test/restaurant.test.js
@@ -1,29 +1,21 @@
 import Restaurant from "../src/node/restaurant.js" 
 import util from "./util.js"
 
-var fulfill2 = {
-    "Kosher": false,
-    "Vegan": false,
-    "Nut": false
-}
-var fulfill3 = {
-    "Kosher": true,
-    "Vegan": true,
-    "Nut": true,
-    "Lactose Intolerant": true,
-    "Gluten Free": true
-}
-
 test('Dummy restaurant 1', () => {
     const fulfill = {
         "Kosher": true,
         "Vegan": false,
         "Nut": false
     }
-    const dummy = new Restaurant("FlavorCo", [142.4, 21.5], fulfill1)
+    const dummy = new Restaurant("FlavorCo", [142.4, 21.5], fulfill);
     expect(dummy.fulfills("Kosher")).toBeTruthy();
     expect(dummy.fulfills("Vegan")).toBeFalsy();
     expect(dummy.fulfills("Nut")).toBeFalsy();
+    const gaf = dummy.getAllFulfills();
+    expect(util.arr_equal_no_order(
+        ["Kosher"],
+         gaf))
+    .toBeTruthy();
 })
 
 test('Dummy restaurant 2', () => {
@@ -32,31 +24,34 @@ test('Dummy restaurant 2', () => {
         "Vegan": false,
         "Nut": false
     }
-    const dummy = new Restaurant("Leamsi Cooking Ltd", [12.5, 21.3], fulfill1)
-    expect(dummy.fulfills("Kosher")).toBeTruthy();
+    const dummy = new Restaurant("Leamsi Cooking Ltd", [12.5, 21.3], fulfill);
+    expect(dummy.fulfills("Kosher")).toBeFalsy();
     expect(dummy.fulfills("Vegan")).toBeFalsy();
     expect(dummy.fulfills("Nut")).toBeFalsy();
+    const gaf = dummy.getAllFulfills();
+    expect(util.arr_equal_no_order(
+        [],
+         gaf))
+    .toBeTruthy();
 })
 
 test('Dummy restaurant 3', () => {
-
+    const fulfill = {
+        "Kosher": true,
+        "Vegan": true,
+        "Nut": true,
+        "Lactose Intolerant": true,
+        "Gluten Free": true
+    }
+    const dummy = new Restaurant("Leamsi Cooking Ltd", [12.5, 21.3], fulfill);
+    expect(dummy.fulfills("Kosher")).toBeTruthy();
+    expect(dummy.fulfills("Vegan")).toBeTruthy();
+    expect(dummy.fulfills("Nut")).toBeTruthy();
+    expect(dummy.fulfills("Lactose Intolerant")).toBeTruthy();
+    expect(dummy.fulfills("Gluten Free")).toBeTruthy();
+    const gaf = dummy.getAllFulfills();
+    expect(util.arr_equal_no_order(
+        ["Kosher", "Vegan", "Nut", "Lactose Intolerant", "Gluten Free"],
+         gaf))
+    .toBeTruthy();
 })
-
-var dummy2 = new Restaurant("Leamsi Cooking Ltd", [12.5, 21.3], fulfill2)
-var dummy3 = new Restaurant("Godspeed You", [13, 12.1], fulfill3)
-
-console.assert(dummy1.fulfills("Kosher") === true)
-console.assert(dummy1.fulfills("Vegan") === false)
-console.assert(dummy1.fulfills("Lactose Intolerant") === false)
-
-console.assert(dummy2.fulfills("Kosher") === false)
-console.assert(dummy2.fulfills("Vegan") === false)
-console.assert(dummy2.fulfills("Lactose Intolerant") === false)
-
-var gaf1 = dummy1.getAllFulfills()
-console.assert(util.arr_equal_no_order(["Kosher"], gaf1), gaf1)
-var gaf2 = dummy2.getAllFulfills()
-console.assert(util.arr_equal_no_order([], gaf2))
-var gaf3 = dummy3.getAllFulfills()
-console.assert(util.arr_equal_no_order(["Kosher", "Vegan", "Nut", "Lactose Intolerant", "Gluten Free"], gaf3))
-
